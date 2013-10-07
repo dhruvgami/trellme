1e89ff635d35196d1783c1ff41c306e0a589cb25
Carlos León <mail@carlosleon.info>
2013-10-07 17:40:16 +0400

    Using grunt for compiling the distribution Javascript file.

---
+++ client/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg : grunt.file.readJSON('package.json'),
+    concat : {
+      js : {
+        src : [
+          'vendor/angular/angular.js',
+          'vendor/**/*.js',
+          'src/app/trellme.js',
+          'src/**/*.js'
+        ],
+        dest : 'trellme.js'
+      }
+    },
+    jshint : {
+      all : ['Gruntfile.js', 'src/**/*.js', 'test/unit/**/*.js']
+    },
+    watch : {
+      scripts : {
+        files : ['src/**/*.js', 'vendor/**/*.js'],
+        tasks : ['jshint', 'concat:js']
+      },
+      extras : {
+        files : ['Gruntfile.js', 'test/unit/**/*.js'],
+        tasks : ['jshint']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
