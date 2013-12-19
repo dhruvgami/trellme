2e74bb1823f74576b7f9f43f4ca3e2d66fca041e
Carlos León <mail@carlosleon.info>
2013-12-19 17:15:06 -0500

    Cleaning up index.html file.

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  $(function () {
+  $(function() {
     $(window).scroll(function(){
       // add navbar opacity on scroll
       if ($(this).scrollTop() > 100) {
@@ -45,5 +45,36 @@
       //IE7 and 8 stuff
       $("body").addClass("old-ie");
     }
+
+    $('a.login-window').click(function() {
+      //Getting the variable's value from a link
+      var loginBox = $(this).attr('href');
+
+      //Fade in the Popup
+      $(loginBox).fadeIn(300);
+
+      //Set the center alignment padding + border see css style
+      var popMargTop = ($(loginBox).height() + 24) / 2;
+      var popMargLeft = ($(loginBox).width() + 24) / 2;
+
+      $(loginBox).css({
+        'margin-top' : -popMargTop,
+        'margin-left' : -popMargLeft
+      });
+
+      // Add the mask to body
+      $('body').append('<div id="mask"></div>');
+      $('#mask').fadeIn(300);
+
+      return false;
+    });
+
+    // When clicking on the button close or the mask layer the popup closed
+    $('a.close, #mask').on('click', function() {
+      $('#mask , .login-popup').fadeOut(300 , function() {
+        $('#mask').remove();
+      });
+      return false;
+    });
   });
 }());
